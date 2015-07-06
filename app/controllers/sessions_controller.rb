b8ad02156490ba4f0863d5dd95a4d9d19a3411ea
Erin Collins & Jess Engel <student@epicodus.com>
2015-07-06 10:32:54 -0700

    sign up, login, log out functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    #try to authenticate the user - if they authenticate successfully, an instance of the User model is returned
+    @user = User.authenticate(params[:email], params[:password])
+    #if an instance is returned and @user is not nil...
+    if @user
+      flash[:notice] = "You're in!"
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      flash[:alert] = "Uh oh, problems."
+      redirect_to "/log-in"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You're out of here."
+    redirect_to "/"
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome!"
+      redirect_to "/"
+    else
+      flash[:notice] = "There was a problem creating your account please try again!"
+      redirect_to :back
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 end
