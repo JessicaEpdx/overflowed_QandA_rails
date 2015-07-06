7da7b8a83defb2a489141a0e6cb09a5ca2a9a87e
Erin Collins & Jess Engel <student@epicodus.com>
2015-07-06 13:52:43 -0700

    integration testing for login

--- spec/features/sign_in_path_spec.rb
+++ spec/features/sign_in_path_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 describe 'sign in form path' do
   it 'validates user and password, shows user navbar' do
+    User.create({username: 'MommaMia', password:'whoayes', password_confirmation:'whoayes', email:'Momma@mia.com'})
     visit '/log-in'
-    click_on 'Post some LINX'
-    fill_in 'Video url', :with => 'https://www.youtube.com/watch?v=aKdV5FvXLuI'
-    fill_in 'Title', :with => 'D-RAD droppin bombs!'
-    fill_in 'Description', :with => 'Un. Real.'
-    click_on 'Create Post'
-    expect(page).to have_content 'D-RAD'
+    fill_in 'username', :with => 'MommaMia'
+    fill_in 'password', :with => 'whoayes'
+    click_on 'Log in'
+    expect(page).to have_content 'MommaMia'
+  end
+  it 'gives error on bad login' do
+    visit '/log-in'
+    fill_in 'username', :with => 'MommaMia'
+    fill_in 'password', :with => 'whoayes'
+    click_on 'Log in'
+    expect(page).to have_content 'Uh oh, problems.'
   end
 end
