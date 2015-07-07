57194155773429e449e8f1db45d5ce46d4c2725b
Erin Collins & Jessica Engel <student@epicodus.com>
2015-07-07 10:44:55 -0700

    profile picture fits in column, admin boolean added to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706215448) do
+ActiveRecord::Schema.define(version: 20150707172547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150706215448) do
     t.integer "cycle_length"
     t.string  "preference"
     t.string  "bio"
+    t.boolean "admin",         default: false
   end
 
 end
